4aae3451ef8d4d9c1f46e745cf1bce44050c53fd
Tim <teenyoung42@gmail.com>
2018-02-24 14:26:24 +1300

    Timer
    add a Timer class

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Timer : MonoBehaviour {
+
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+
+    public TimeSpan CalculateDuration()
+    {
+        TimeSpan duration = EndTime.Subtract(StartTime);
+        return duration;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
