b82678e5a916eacd14da86931028723457f1fb11
Ya <yasmine.WenY@gmail.com>
2018-02-26 17:29:44 +1300

    Counter Button

---
+++ Assets/Scripts/Counter_Button.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Counter_Button : MonoBehaviour {
+
+    public Button inputEnter;
+    public Text totalNumText, singleNumText;
+    public InputField inputNumField;
+
+    public string unit;
+
+    private int number, totalNum; 
+    
+    // Use this for initialization
+    void Start() {
+        inputEnter.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void EnterToInput()
+    {
+        inputEnter.gameObject.SetActive(true);
+    }
+
+    public void EnterToShowTotal()
+    {
+        inputEnter.gameObject.SetActive(false);
+        //number = Convert.ToInt32(singleNumText.text);
+        number = Convert.ToInt32(inputNumField.text);
+        totalNum += number;
+        totalNumText.text = totalNum + " " + unit;
+
+        // ??find a way to rest to default 
+        inputNumField.text = "";
+        
+    }
+    
+}
+
