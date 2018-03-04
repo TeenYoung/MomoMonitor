29d74ab5c5c765ee6f64188f6dee79baad6a70de
Tim <teenyoung42@gmail.com>
2018-03-04 14:08:48 +1300

    add a panel: timer input

---
+++ Assets/Scripts/Panel_Input.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel_Input : MonoBehaviour {
+
+    public void InputFieldOnEndEdit()
+    {
+
+    }
+
+}
