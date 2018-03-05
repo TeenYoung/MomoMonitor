33df4fc2f2d2c56441303e24cb7a40daf6f6eb17
Ya <yasmine.WenY@gmail.com>
2018-03-05 19:53:08 +1300

    Setting Button

---
+++ Assets/Scripts/Setting_Button.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Setting_Button : MonoBehaviour
+{
+
+    public Button NewDay_Button, Reset_Button;
+    public GameObject Setting_Panel;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))Cancel();
+    }
+
+    public void SettingOnClick()
+    {
+        Setting_Panel.gameObject.SetActive(true);
+        NewDay_Button.gameObject.SetActive(true);
+        Reset_Button.gameObject.SetActive(true);
+    }
+
+    public void Cancel()
+    {
+        Setting_Panel.gameObject.SetActive(false);
+        NewDay_Button.gameObject.SetActive(false);
+        Reset_Button.gameObject.SetActive(false);
+    }
+
+}
