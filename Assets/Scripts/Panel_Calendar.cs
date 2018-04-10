3c2bee09e5488b9d5d67ee75d7e828f9a0b45931
Ya <yasmine.WenY@gmail.com>
2018-04-10 17:41:11 +1200

    calendar v0.7
    show special age of weeks if age less than 3 month

--- Assets/Scripts/Panel_Calendar.cs
+++ Assets/Scripts/Panel_Calendar.cs
@@ -13,9 +13,11 @@ public class Panel_Calendar : MonoBehaviour {
     public GameObject previousMonthButton, nextMonthButton, backToTodayButton;
     private DateTime today, DateTime_babyBirth;
     private int  monthTemp, dayTemp, daysInMonths, yearTemp, firstDayIndex;
-    private string babyBirth, weekOfFirstDay; 
+    private string babyBirth, weekOfFirstDay;
 
-    private void OnEnable()
+    
+
+    public void OnEnable()
     {
         today = DateTime.Now;
         babyBirth = PlayerPrefs.GetString("babyBirth");
@@ -36,85 +38,106 @@ public class Panel_Calendar : MonoBehaviour {
         if (week == DayOfWeek.Friday.ToString()) num = 5;
         else if(week == DayOfWeek.Saturday.ToString()) num = 6;
         return num;
-    }      
+    }    
 
-    public void SetSpecialAge(GameObject gob, int day, int yearTemp, int monthTemp, int i)
-    {
-        gob.transform.Find("Text_SpecialAge").gameObject.SetActive(false);
-        if ((yearTemp - DateTime_babyBirth.Year) * 12 + monthTemp - DateTime_babyBirth.Month >= 0 && day == DateTime_babyBirth.Day) //根據年齡顯示特殊日期，大於一個月顯示月，一年内按整月顯示，大於一年顯示年和周
-        {
-            gob.transform.Find("Text_SpecialAge").gameObject.SetActive(true);
-            if ((yearTemp - DateTime_babyBirth.Year) * 12 + monthTemp - DateTime_babyBirth.Month < 12) //age小於1嵗，
-            {
-                gob.transform.Find("Text_SpecialAge").GetComponent<Text>().text = (yearTemp - DateTime_babyBirth.Year) * 12 + monthTemp - DateTime_babyBirth.Month + " Month";
-            }
-            if (yearTemp == DateTime_babyBirth.Year && monthTemp == DateTime_babyBirth.Month) gob.transform.Find("Text_SpecialAge").GetComponent<Text>().text = "Birthday";
-            if ((yearTemp - DateTime_babyBirth.Year) * 12 + monthTemp - DateTime_babyBirth.Month >= 12) //age大於等於1嵗時，顯示整年和整月，月為0時衹顯示年
-            {
-                if (monthTemp == DateTime_babyBirth.Month) gob.transform.Find("Text_SpecialAge").GetComponent<Text>().text = (yearTemp - DateTime_babyBirth.Year) + " Year ";
-                else gob.transform.Find("Text_SpecialAge").GetComponent<Text>().text = (((yearTemp - DateTime_babyBirth.Year) * 12 + monthTemp - DateTime_babyBirth.Month)) / 12 + " Y "
-                + (((yearTemp - DateTime_babyBirth.Year) * 12 + monthTemp - DateTime_babyBirth.Month)) % 12 + " M";
-            }            
-        }
-        //else if ((yearTemp - DateTime_babyBirth.Year) * 12 + monthTemp - DateTime_babyBirth.Month <= 3 && new DateTime(yearTemp, monthTemp, day) > DateTime_babyBirth) //小於三個月顯示周
-        //{
-        //    if ((new DateTime(yearTemp, monthTemp, day).Subtract(DateTime_babyBirth).Days + 1) % 7 == 0 && (new DateTime(yearTemp, monthTemp, day).Subtract(DateTime_babyBirth).Days) / 7 > 0)
-        //    {
-        //        gob.transform.Find("Text_SpecialAge").gameObject.SetActive(true);
-        //        gob.transform.Find("Text_SpecialAge").GetComponent<Text>().text = (new DateTime(yearTemp, monthTemp, day).Subtract(DateTime_babyBirth).Days) / 7 + " Week";
-        //    }
-        //}
-    }
 
-    public void SetCalender(int yearTemp, int monthTemp)
+    public void SetCalender(int thisYear, int thisMonth)
     {
-        int daysInMonths, firstDayIndex, dayTemp;         
-        daysInMonths = DateTime.DaysInMonth(yearTemp, monthTemp);
-        weekOfFirstDay = new DateTime(yearTemp, monthTemp, 1).DayOfWeek.ToString();
+        int daysInThisMonths, firstDayIndex, lastMonth, lastYear,nextMonth, nextYear; //lastMonth, lastYear為日曆中上個月部分的入參，nextMonth, nextYear為日曆下個月部分的入參
+        daysInThisMonths = DateTime.DaysInMonth(thisYear, thisMonth);
+        weekOfFirstDay = new DateTime(thisYear, thisMonth, 1).DayOfWeek.ToString();
         firstDayIndex = ChangeWeekIntoNum(weekOfFirstDay);
-        if(yearTemp!=DateTime.Now.Year) month.text = new DateTime(yearTemp, monthTemp, 1).ToString("MMMM", new CultureInfo("en-us")) //如不是當年月份，同時在月份後顯示年份
-                + " " + yearTemp;
-        else month.text = new DateTime(yearTemp, monthTemp, 1).ToString("MMMM", new CultureInfo("en-us")); //月份轉爲字母顯示
-        int j = 1;
-        dayTemp = DateTime_babyBirth.Day;
-        for (int i = firstDayIndex; i <= daysInMonths + firstDayIndex ; i++) //填充當月日期,j為日期
+        //foreach(GameObject days in days)
+        //{
+        //    days.transform.Find("Text_SpecialAge").gameObject.SetActive(false);
+        //    //days.transform.Find("Text_SpecialAge").GetComponent<Text>().text = "SpecialAge";
+        //    //days.transform.Find("Text_SpecialAge").GetComponent<Text>().color = Color.clear;
+        //}                
+        if (thisYear!=DateTime.Now.Year) month.text = new DateTime(thisYear, thisMonth, 1).ToString("MMMM", new CultureInfo("en-us")) //如不是當年月份，同時在月份後顯示年份
+                + " " + thisYear;
+        else month.text = new DateTime(thisYear, thisMonth, 1).ToString("MMMM", new CultureInfo("en-us")); //月份轉爲字母顯示
+        if (thisMonth == 1)  //賦值日曆中上個月部分的入參，本月為1月時，上個月為12月，年份-1
         {
-            days[i].transform.Find("Text_Day").GetComponent<Text>().text = j.ToString();
-            //if (new DateTime(yearTemp, monthTemp, j) == DateTime.Today) days[i].gameObject.GetComponent<Image>().color = Color.gray;
-            days[i].transform.Find("Text_Day").GetComponent<Text>().color = Color.black; //當前月字體為黑色
-            SetSpecialAge(days[i], j, yearTemp,monthTemp, i);            
-            j++;            
+            lastYear = thisYear - 1;
+            lastMonth = 12;
         }
-        if (monthTemp == 1)
+        else
         {
-            yearTemp--;
-            monthTemp = 12;
+            lastMonth = thisMonth - 1;
+            lastYear = thisYear;
         }
-        else monthTemp--;
-        j = DateTime.DaysInMonth(yearTemp, monthTemp);        
+        if (thisMonth == 12) //賦值日曆下個月部分的入參，本月為12月時，下個月為1月，年份+1
+        {
+            nextYear = thisYear + 1;
+            nextMonth = 1;
+        }
+        else
+        {
+            nextMonth = thisMonth + 1;
+            nextYear = thisYear;
+        } 
+        //dayTemp = DateTime_babyBirth.Day;             
+        int j = DateTime.DaysInMonth(lastYear, lastMonth);        
         for (int i = firstDayIndex - 1; i >= 0; i--) //填充上月末幾日
         {
             days[i].transform.Find("Text_Day").GetComponent<Text>().text = j.ToString();
+            //if (yearTemp == DateTime_babyBirth.Year && monthTemp == DateTime_babyBirth.Month && dayTemp == DateTime_babyBirth.Day) weekOfBirth = i;
             days[i].transform.Find("Text_Day").GetComponent<Text>().color = Color.gray; //非當前月字體為灰色            
-            SetSpecialAge(days[i], j, yearTemp, monthTemp, i);
+            SetSpecialAge(days[i], j, lastYear, lastMonth, DateTime_babyBirth);
             j--;
         }
         j = 1;
-        if (monthTemp == 12)
-        {
-            yearTemp++;
-            monthTemp = 1;
-        }
-        else monthTemp++;
-        for (int i = daysInMonths + firstDayIndex ; i < days.Count; i++) //填充下月初幾日
+        for (int i = firstDayIndex; i < daysInThisMonths + firstDayIndex; i++) //填充當月日期,j為日期
         {
             days[i].transform.Find("Text_Day").GetComponent<Text>().text = j.ToString();
-            days[i].transform.Find("Text_Day").GetComponent<Text>().color = Color.gray; //非當前月字體為灰色            
-            SetSpecialAge(days[i], j, yearTemp, monthTemp+1, i);
+            //if (yearTemp == DateTime_babyBirth.Year && monthTemp == DateTime_babyBirth.Month && j == DateTime_babyBirth.Day) weekOfBirth = i;
+            //if (new DateTime(yearTemp, monthTemp, j) == DateTime.Today) days[i].gameObject.GetComponent<Image>().color = Color.gray;
+            days[i].transform.Find("Text_Day").GetComponent<Text>().color = Color.black; //當前月字體為黑色
+            SetSpecialAge(days[i], j, thisYear, thisMonth, DateTime_babyBirth);
             j++;
-        }        
+        } 
+        j = 1;
+        for (int i = daysInThisMonths + firstDayIndex; i < days.Count; i++) //填充下月初幾日
+        {
+            days[i].transform.Find("Text_Day").GetComponent<Text>().text = j.ToString();
+            //if (yearTemp == DateTime_babyBirth.Year && monthTemp == DateTime_babyBirth.Month && dayTemp == DateTime_babyBirth.Day) weekOfBirth = i;
+            days[i].transform.Find("Text_Day").GetComponent<Text>().color = Color.gray; //非當前月字體為灰色            
+            SetSpecialAge(days[i], j, nextYear, nextMonth, DateTime_babyBirth);
+            j++;
+        }
     }
-   
+
+    public void SetSpecialAge(GameObject gob, int day, int thisYear, int thisMonth, DateTime DateTime_babyBirth)
+    {
+        gob.transform.Find("Text_SpecialAge").gameObject.SetActive(false);
+        DateTime tempDT = new DateTime(thisYear, thisMonth, day);
+        if ((thisYear - DateTime_babyBirth.Year) * 12 + thisMonth - DateTime_babyBirth.Month >= 0 && day == DateTime_babyBirth.Day) //根據年齡顯示特殊日期，大於一個月顯示月，一年内按整月顯示，大於一年顯示年和周
+        {
+            gob.transform.Find("Text_SpecialAge").gameObject.SetActive(true); 
+            if ((thisYear - DateTime_babyBirth.Year) * 12 + thisMonth - DateTime_babyBirth.Month < 12) //age小於1嵗，
+            {
+                gob.transform.Find("Text_SpecialAge").GetComponent<Text>().text = (thisYear - DateTime_babyBirth.Year) * 12 + thisMonth - DateTime_babyBirth.Month + " Month";
+                
+            }
+            if (thisYear == DateTime_babyBirth.Year && thisMonth == DateTime_babyBirth.Month)gob.transform.Find("Text_SpecialAge").GetComponent<Text>().text = "Birthday"; //生日當天
+            if ((thisYear - DateTime_babyBirth.Year) * 12 + thisMonth - DateTime_babyBirth.Month >= 12) //age大於等於1嵗時，顯示整年和整月，月為0時衹顯示年
+            {
+                if (thisMonth == DateTime_babyBirth.Month) gob.transform.Find("Text_SpecialAge").GetComponent<Text>().text = (thisYear - DateTime_babyBirth.Year) + " Year ";
+                else gob.transform.Find("Text_SpecialAge").GetComponent<Text>().text = (((thisYear - DateTime_babyBirth.Year) * 12 + thisMonth - DateTime_babyBirth.Month)) / 12 + " Y "
+                + (((thisYear - DateTime_babyBirth.Year) * 12 + thisMonth - DateTime_babyBirth.Month)) % 12 + " M";
+            }
+        }
+        if (tempDT.Subtract(DateTime_babyBirth.Date).Days < 91 && tempDT > DateTime_babyBirth)// && gob.transform.Find("Text_SpecialAge").GetComponent<Text>().text == "SpecialAge") //小於三個月顯示周
+        {
+            if ((tempDT.Subtract(DateTime_babyBirth.Date).Days) % 7 == 0 && (tempDT.Subtract(DateTime_babyBirth.Date).Days) / 7 > 0)
+            {
+                print(tempDT);
+                gob.transform.Find("Text_SpecialAge").gameObject.SetActive(true);
+                gob.transform.Find("Text_SpecialAge").GetComponent<Text>().text = tempDT.Subtract(DateTime_babyBirth.Date).Days / 7 + " Week";
+            }
+        }
+    }
+
     public void ToNextMonth() //button click ToNextMonth
     {
         if (monthTemp == 12)
